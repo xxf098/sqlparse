28b2f58a3e1f0f880e2d5cc5f1b6051a3712d847
xxf098 <xxf098@protonmail.com>
2022-06-28 22:53:23 -0400

    complex_query

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,11 +4,23 @@ use sqlparse::{FormatOption, Formatter};
 fn simple_query(c: &mut Criterion) {
     let sql = "SELECT * FROM my_table WHERE id = 1";
     let mut f = Formatter::default();
-    let mut formatter = FormatOption::default();
-    formatter.reindent = true;
+    let mut formatter = FormatOption::default_reindent();
     formatter.reindent_aligned = true;
     f.build_filters(&mut formatter);
     c.bench_function("simple query", |b| {
+        b.iter(|| {
+            f.format_sql(black_box(sql), black_box(&formatter));
+        })
+    });
+}
+
+fn complex_query(c: &mut Criterion) {
+    let sql = "SELECT t1.id, t1.name, t1.title, t1.description, t2.mothers_maiden_name, t2.first_girlfriend\nFROM my_table t1 LEFT JOIN other_table t2 ON t1.id = t2.other_id WHERE t2.order BETWEEN  17 AND 30";
+    let mut f = Formatter::default();
+    let mut formatter = FormatOption::default_reindent();
+    formatter.reindent_aligned = true;
+    f.build_filters(&mut formatter);
+    c.bench_function("complex query", |b| {
         b.iter(|| {
             f.format_sql(black_box(sql), black_box(&formatter));
         })
@@ -19,5 +31,6 @@ fn simple_query(c: &mut Criterion) {
 criterion_group!(
     benches,
     simple_query,
+    complex_query,
 );
 criterion_main!(benches);
